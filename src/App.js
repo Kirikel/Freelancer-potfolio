4994f8c9e1c40d3f07daeea1b66b507df383fc62
Kirikel <Mihaylenko04@gmail.com>
2023-04-26 13:14:23 +0500

    comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Project from "./pages/Project";
+import Contacts from "./pages/Contacts";
+
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <ScrollToTop/>
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/projects" element={<Projects/>}/>
+              <Route path="/contacts" element={<Contacts/>}/>
+              <Route path="/project/:id" element={<Project/>}/>
+          </Routes>
+          <Footer/>
+        </Router>
     </div>
   );
 }
---
+++ src/utils/detectDarkMode.js
@@ -0,0 +1,11 @@
+const detectDarkMode = () => {
+	if (
+		window.matchMedia &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches
+	) {
+		return 'dark'
+	}
+    return 'light'
+};
+
+export default detectDarkMode;
